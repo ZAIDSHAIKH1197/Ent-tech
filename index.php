b9ae1f82858ee9184bba1a2cd4d487ffe3a007e1
ZAIDSHAIKH1197 <41756915+ZAIDSHAIKH1197@users.noreply.github.com>
2018-07-26 23:50:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,416 @@
+<?php  include('header.php');?>
+<?php include('slider.php');?>
+
+ <div class="top-bg" >
+	<div class="container">
+		<div class="row" >
+			<div class="col-md-4 col-xm-4 top-fullbox" >
+				<div class="top-box text-center" style="border-right:0px solid #cfcfcf;">
+					<h4>QUICK CONTACT</h4>
+					<p>CALL US ON +91 9768018490</p>
+				</div>
+			</div>
+			<div class="col-md-4 col-xm-4 top-fullbox"  >
+				<div class="top-box text-center" style="border-right:0px solid #cfcfcf;">
+					<h4>SPECIAL DISCOUNT</h4>
+					<p>FOr refrel</p>
+				</div>
+			</div>
+			<div class="col-md-4 col-xm-4 top-fullbox" >
+				<div class="top-box text-center">
+					<h4>FIRST VISIT</h4>
+					<p>FIRST TRIAL IS FREE!</p>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
+
+<!--
+<div class="top-bg" >
+	<div class="container">
+		<div class="row quick-box" >
+		<div class="col-md-12 text-center">
+			<ul class="top-fullbox"]>
+				<li >
+				<div class="top-box text-center" style="border-right:0px solid #cfcfcf;">
+					<h4>QUICK CONTACT</h4>
+					<p>CALL +91 0000000000</p>
+				</div>
+				</li>
+				<li>
+				<div class="top-box text-center" style="border-right:0px solid #cfcfcf;">
+					<h4>SPECIAL DISCOUNT</h4>
+					<p>FOR COUPLE TRAINING</p>
+				</div>
+				</li>
+				<li>
+				<div class="top-box text-center">
+					<h4>FIRST VISIT</h4>
+					<p>FIRST TRIAL IS FREE!</p>
+				</div>
+				</li>
+			</ul>
+			
+		</div>
+		</div>
+	</div>
+</div>   -->
+
+
+
+<div class="">
+<div class="container bordr main-col">
+	<div class="row">
+		<div class="col-md-12 approach-sec text-center">
+			<div class="approach-head" >OUR APPROACH</div>
+			<div class="separator small center" ></div>
+			<p>Entertainment technology is the discipline of using manufactured or created components to enhance or make possible any sort of entertainment experience. Because entertainment categories are so broad, and because entertainment models the world in many ways, the types of implemented technology are derived from a variety of sources.</p>
+			<p>Entertainment can be classified into many cateogaries such as dance,music,art,drama and many more.</p>
+			<p>Dance is a performing art form consisting of purposefully selected sequences of human movement.Different types of dance form are as follows classical,contemporary,folk and tribal dance forms and many more down the line.</p>
+			<p>Music is an art form and cultural activity whose medium is sound organized in time. The common elements of music are pitch, rhythm, dynamics, and the sonic qualities of timbre and texture. Music can be further cateogarized as classical,vocal,instrumental and many more.</p>
+			<p>Art is a diverse range of human activities in creating visual, auditory or performing artifacts by authors imagination.Art is further classified into sketching, movies, short film, sculpture, drama and many more down the line.</p>
+			<p>Technology is  defined as: "...principles, processes, and nomenclatures of the more conspicuous arts, particularly those which involve applications of science, and which may be considered useful, by promoting the benefit of society, together with the emolument.</p>
+			<p>There are various fields in which technology is used and some of them are stated below,they are as follows Natural sciences, Engineering and Technology, Engineering and Technology, Agricultural sciences, Humanities, and many more.</p>
+			
+		</div>
+	</div>
+</div>
+</div>
+
+<div class="mat-bg">
+<div class="container main-col">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="join-us text-center">
+			<div class="join-text">Every picture has a story and feelings to tell....</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
+
+
+<section style="background:;">
+	<div class="container" style="padding:30px 0px;">
+		<div class="row box">
+			<!--<ul>
+				<li><img src="assets/images/yogaicons/yogaicon9.png" ><p>dolor sit amet, consectetur adipiscing elit.</p></li>
+				<li><img src="assets/images/yogaicons/yogaicon9.png" ><p>dolor sit amet, consectetur adipiscing elit.</p></li>
+				<li><img src="assets/images/yogaicons/yogaicon9.png" ><p>dolor sit amet, consectetur adipiscing elit.</p></li>
+				<li><img src="assets/images/yogaicons/yogaicon9.png" ><p>dolor sit amet, consectetur adipiscing elit.</p></li>
+			</ul>-->
+			<div class="col-md-4">
+				<div class="yogaicon text-center ">
+					<img class="yogacircle" src="assets/images/ICONS/LOGO1.jpg" >
+					<p>Entertainment</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="yogaicon text-center ">
+					<img class="yogacircle" src="assets/images/ICONS/LOGO2.jpg" >
+					<p>Technology</p>
+				</div>
+			<!--<div class="box2 main text-center bordr">
+				<h2>Product 2</h2>
+
+				</div> -->
+			</div>
+			<div class="col-md-4">
+				<div class="yogaicon text-center">
+					<img class="yogacircle" src="assets/images/ICONS/LOGO3.jpg" >
+					<p>Animatronics</p>
+				</div>
+			</div>
+			
+			</div> 
+		</div>
+	</div>
+</section>
+
+
+
+<div class="">
+	<div class="container">
+		<div class="row pan">
+			<div class="col-md-4">
+				<div class="panel text-center">
+					<!--<div class="panel panel-content text-center">-->
+						<h3>Entertainment</h3>
+						<p>Entertainment is a form of activity that holds the attention and interest of an audience, or gives pleasure and delight.It can be an idea or a task, but is more likely to be one of the activities or events that have developed over thousands of years specifically for the purpose of keeping an audience's attention.Story telling,music, art, craft are also means of entertainment.</p>
+					<!--</div>-->
+				<!--	<div class="panel panel-footer">
+						<div class="pill-left">
+							Lorem Ipsum Dolar
+						</div>
+						<button class="btn btn-info pull-right">Read More...</button>
+					</div> -->
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel text-center">
+					<!--<div class="panel panel-content text-center">-->
+						<h3>Technology</h3>
+						<p>Technology can be defined as the application of knowledge for practical ends.Tecnology is widely spread in all fields ranging from natural sciences to agriculture to humanities.Technology deals with both material and immaterial entities based on application of mental and physical effort.</p>
+				<!--	</div> -->
+				<!--	<div class="panel panel-footer text-center">
+						
+						<button class="btn btn-info">Read More...</button>
+					</div> -->
+				</div>
+		
+			</div>
+			<div class="col-md-4">
+				<div class="panel text-center">
+					<!--<div class="panel panel-content text-center">-->
+						<h3>Animatronics </h3>
+						<p>Animatronics uses robotic devices to emulate a human or an animal, or bring lifelike characteristics to an otherwise inanimate object.Applications in movie special effects and theme parks for entertainment purpose.It is a multi-disciplinary field which integrates anatomy, robots, mechatronics, and puppetry resulting in lifelike animation.</p>
+					<!--</div>-->
+				<!--	<div class="panel panel-footer">
+						<div class="pill-left">
+							Lorem Ipsum Dolar
+						</div>
+						<button class="btn btn-info pull-right">Read More...</button>
+					</div> -->
+				</div>
+			</div>	
+            		
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+<div class="upcoming-bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9 bordr main-col">
+				<div class="content-head2 text-center" style="font-size:22px;color:#EA5033;">UPCOMING EVENTS</div>
+					<!--<div class="separator small center" style="color:#fff;"></div>-->
+							<div class="news-content">
+								<ul>
+									<marquee class="news" behavior="scroll" direction="up" scrollamount="4" onmouseover="this.stop();" onmouseout="this.start();">
+									<li>Stock market update:HDFC Bank,HDFC,L&T Infotech hit fresh 52-week high</li>
+									<li> IoT and AI to fundamentally change the way we live and work: CSG</li>
+									<li>MAD' Coming to Snapchat in Partnership Between DC and Snap</li>
+									<li>BBC opens online store for shows,top gear and sherlock in the list</li>
+									<li>The rates of petrol increased by 2 rupees</li>
+									<li>Infosys says increased rejection of work visa applications may cause delays, raise project costs for clients</li>
+									<li>Raiots in Maharashtra related to maratha reservation.</li>
+									<li>India's exports record highest growth in 2017-18</li>
+									</marquee>
+								</ul>
+							</div>
+				
+			</div>
+			<div class="col-md-3 bordr main-col">
+				<?php include('quick-contact.php');?>
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+
+<!--
+	<div class="container main-col"> 
+		<div class="row">
+			<div class="col-md-8">
+				<div class="col-md-12">
+					<div class="row">
+						<div class="row heading-box">
+						<div class="col-md-12">
+							<div class=" main bordr">
+								<div class="content-head">Heading</div>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ex velit, molestie quis justo ac, tincidunt porta ante. In rutrum lectus sed purus viverra sodales. Quisque ac lacus sed nisl blandit ultrices in nec tortor. Proin pretium in enim porta viverra. Praesent finibus, odio vitae gravida tristique, lorem ligula feugiat lacus, et sagittis dui augue sit amet odio. Proin libero dolor, ullamcorper pellentesque nisl id, porttitor molestie leo. Maecenas pulvinar leo metus, a luctus urna aliquam at. Quisque in posuere ex. Nulla facilisi. Nam consequat, metus sed mattis viverra, tellus ex fringilla dui, a euismod lectus neque in nibh. Quisque sollicitudin varius tortor nec lobortis. Ut quis massa nec ex semper tincidunt ut in arcu. Ut scelerisque est sit amet iaculis gravida.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ex velit, molestie quis justo ac, tincidunt porta ante. </p>
+								<button class="read-button">Read More...</button>
+							</div>
+						</div>
+					</div>
+						<div class="lecture-schedule main bordr">
+							<div class="content-head">Lecture Schedule</div>
+								
+								<div class="schedule-tab">
+								
+								<ul class="nav nav-tabs lecture-tab">
+									<li class="active table-tab1"><a style="background:#85C226 ;" data-toggle="tab" href="#home">Today's Lecture</a></li>
+									<li class="table-tab2"><a style="background:#F8C301;" data-toggle="tab" href="#menu1">Upcoming Lectures</a></li>
+									<li class="table-tab3"><a style="background:#3BB3C3 ;" data-toggle="tab" href="#menu2">Addition Lectures</a></li>
+								</ul>
+
+								<div class="tab-content" style="overflow:hidden;overflow-x: scroll;">
+									<div id="home" class="tab-pane fade in active">
+										<table class="table table-bordered">
+											<thead>
+											  <tr>
+												<th>Date</th>
+												<th>Time</th>
+												<th>Course</th>
+												<th>Subject</th>
+												<th>Faculty</th>
+											  </tr>
+											</thead>
+											<tbody>
+											  <tr>
+												<td>20-5-2016</td>
+												<td>5.00PM</td>
+												<td>Web Desinging</td>
+												<td>HTML</td>
+												<td>Prof. ABCD</td>
+											  </tr>
+											   <tr>
+												<td>20-5-2016</td>
+												<td>5.00PM</td>
+												<td>Web Desinging</td>
+												<td>HTML</td>
+												<td>Prof. ABCD</td>
+											  </tr>
+											   <tr>
+												<td>20-5-2016</td>
+												<td>5.00PM</td>
+												<td>Web Desinging</td>
+												<td>HTML</td>
+												<td>Prof. ABCD</td>
+											  </tr>
+											</tbody>
+										  </table>
+									</div>
+									<div id="menu1" class="tab-pane fade">
+										<table class="table table-bordered">
+											<thead>
+											  <tr>
+												<th>Date</th>
+												<th>Time</th>
+												<th>Course</th>
+												<th>Subject</th>
+												<th>Faculty</th>
+											  </tr>
+											</thead>
+											<tbody>
+											  <tr>
+												<td>20-5-2016</td>
+												<td>5.00PM</td>
+												<td>Web Desinging</td>
+												<td>HTML</td>
+												<td>Prof. ABCD</td>
+											  </tr>
+											   <tr>
+												<td>20-5-2016</td>
+												<td>5.00PM</td>
+												<td>Web Desinging</td>
+												<td>HTML</td>
+												<td>Prof. ABCD</td>
+											  </tr>
+											   <tr>
+												<td>20-5-2016</td>
+												<td>5.00PM</td>
+												<td>Web Desinging</td>
+												<td>HTML</td>
+												<td>Prof. ABCD</td>
+											  </tr>
+											</tbody>
+										  </table>
+									</div>
+									<div id="menu2" class="tab-pane fade">
+										<table class="table table-bordered">
+											<thead>
+											  <tr>
+												<th>Date</th>
+												<th>Time</th>
+												<th>Course</th>
+												<th>Subject</th>
+												<th>Faculty</th>
+											  </tr>
+											</thead>
+											<tbody>
+											  <tr>
+												<td>20-5-2016</td>
+												<td>5.00PM</td>
+												<td>Web Desinging</td>
+												<td>HTML</td>
+												<td>Prof. ABCD</td>
+											  </tr>
+											   <tr>
+												<td>20-5-2016</td>
+												<td>5.00PM</td>
+												<td>Web Desinging</td>
+												<td>HTML</td>
+												<td>Prof. ABCD</td>
+											  </tr>
+											   <tr>
+												<td>20-5-2016</td>
+												<td>5.00PM</td>
+												<td>Web Desinging</td>
+												<td>HTML</td>
+												<td>Prof. ABCD</td>
+											  </tr>
+											</tbody>
+										  </table>
+										  
+									</div> 
+								</div>
+								<button class="read-button">Read More...</button>
+							</div>  
+						</div>
+					</div>
+				
+				</div>
+			</div>
+			
+			
+			<div class="col-md-4 main-col">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="main bordr">
+							<form class="enquiry-form" action="" method="post">
+								<div class="content-head2">Enquiry Now</div>
+								<input style="margin-top:10px;"type="text" name="name" id="name" placeholder="Enter Your Name" required><br>
+								<input type="text" name="email" id="email" placeholder="Enter Your E-MailID" required><br>
+								<input type="text" pattern="[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]" name="mobile" id="mobile" placeholder="Enter Your Mobile Number" required><br>
+								<input type="text" name="location" id="location" placeholder="Enter Your Location" required><br>
+								<input type="text" name="course" id="course" placeholder="Enter Your Course" required><br>
+								<button class="enquiry-submit">Submit</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			
+				<div class="row news-update">
+					<div class="col-md-12">
+						<div class="main bordr">
+							<div class="content-head2">News Updates</div>
+							<div class="news-content">
+								<ul>
+									<marquee class="news" behavior="scroll" direction="up" scrollamount="4" onmouseover="this.stop();" onmouseout="this.start();">
+									<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ex velit, molestie quis justo ac, tincidunt porta ante. In rutrum lectus sed purus viverra sodales.</li>
+									<li> Quisque ac lacus sed nisl blandit ultrices in nec tortor. Proin pretium in enim porta viverra.</li>
+									<li>Praesent finibus, odio vitae gravida tristique, lorem ligula feugiat lacus, et sagittis dui augue sit amet odio.</li>
+									<li>Maecenas pulvinar leo metus, a luctus urna aliquam at. Quisque in posuere ex. Nulla facilisi. Nam consequat, metus sed mattis viverra, tellus ex fringilla dui, a euismod lectus neque in nibh.</li>
+									</marquee>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+-->
+		
+
+
+
+<?php include('footer.php');?>
